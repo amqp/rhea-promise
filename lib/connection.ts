aa58f32bf24f32449eec9ac399e7d7637754ebeb
ShivangiReja <45216704+ShivangiReja@users.noreply.github.com>
2019-03-18 16:20:05 -0700

    removeAllSessions() to clean up connection. (#33)
    *  removeAllSessions() to clean up connection.

--- lib/connection.ts
+++ lib/connection.ts
@@ -377,6 +377,15 @@ export class Connection extends Entity {
   }
 
   /**
+   * Remove all the sessions from the internal map.
+   */
+  removeAllSessions(): void {
+    if (this._connection) {
+      this._connection.remove_all_sessions();
+    }
+  }
+
+  /**
    * Determines whether the remote end of the connection is open.
    * @returns {boolean} result `true` - is open; `false` otherwise.
    */

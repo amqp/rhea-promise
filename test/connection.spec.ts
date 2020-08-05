c91fd6c9845f8835918bd2c54114bc7ec9b262ac
Ramya Rao <ramyar@microsoft.com>
2020-08-05 15:31:34 -0700

    Tests for the create* methods on the connection (#64)

--- test/connection.spec.ts
+++ test/connection.spec.ts
@@ -37,6 +37,167 @@ describe("Connection", () => {
     assert.isFalse(connection.isOpen(), "Connection should be closed.");
   });
 
+  it("createSession()", async () => {
+    const connection = new Connection({
+      port: mockServiceListener.address().port,
+    });
+
+    await connection.open();
+    assert.isTrue(connection.isOpen(), "Connection should be open.");
+
+    const session = await connection.createSession();
+    assert.isTrue(session.isOpen(), "Session should be open.");
+
+    await connection.close();
+    assert.isFalse(session.isOpen(), "Session should be not be open after connection closed.");
+  });
+
+  it("createSender()", async () => {
+    const connection = new Connection({
+      port: mockServiceListener.address().port,
+    });
+
+    await connection.open();
+    assert.isTrue(connection.isOpen(), "Connection should be open.");
+
+    const sender = await connection.createSender();
+    assert.isTrue(sender.isOpen(), "Sender should be open.");
+
+    await connection.close();
+    assert.isFalse(sender.isOpen(), "Sender should be not be open after connection closed.");
+  });
+
+  it("createSender() with given session", async () => {
+    const connection = new Connection({
+      port: mockServiceListener.address().port,
+    });
+
+    await connection.open();
+    assert.isTrue(connection.isOpen(), "Connection should be open.");
+
+    const session = await connection.createSession();
+    assert.isTrue(session.isOpen(), "Session should be open.");
+
+    const sender = await connection.createSender({ session });
+    assert.isTrue(sender.isOpen(), "Sender should be open.");
+    assert.equal(sender.session, session, "Session of sender should be the same as the session passed to createSender()");
+
+    await connection.close();
+    assert.isFalse(sender.isOpen(), "Sender should be not be open after connection closed.");
+  });
+
+  it("createAwaitableSender()", async () => {
+    const connection = new Connection({
+      port: mockServiceListener.address().port,
+    });
+
+    await connection.open();
+    assert.isTrue(connection.isOpen(), "Connection should be open.");
+
+    const sender = await connection.createAwaitableSender();
+    assert.isTrue(sender.isOpen(), "Sender should be open.");
+
+    await connection.close();
+    assert.isFalse(sender.isOpen(), "Sender should be not be open after connection closed.");
+  });
+
+  it("createAwaitableSender() with given session", async () => {
+    const connection = new Connection({
+      port: mockServiceListener.address().port,
+    });
+
+    await connection.open();
+    assert.isTrue(connection.isOpen(), "Connection should be open.");
+
+    const session = await connection.createSession();
+    assert.isTrue(session.isOpen(), "Session should be open.");
+
+    const sender = await connection.createAwaitableSender({ session });
+    assert.isTrue(sender.isOpen(), "Sender should be open.");
+    assert.equal(sender.session, session, "Session of sender should be the same as the session passed to createSender()");
+
+    await connection.close();
+    assert.isFalse(sender.isOpen(), "Sender should be not be open after connection closed.");
+  });
+
+  it("createReceiver()", async () => {
+    const connection = new Connection({
+      port: mockServiceListener.address().port,
+    });
+
+    await connection.open();
+    assert.isTrue(connection.isOpen(), "Connection should be open.");
+
+    const receiver = await connection.createReceiver();
+    assert.isTrue(receiver.isOpen(), "Receiver should be open.");
+
+    await connection.close();
+    assert.isFalse(receiver.isOpen(), "Receiver should be not be open after connection closed.");
+  });
+
+  it("createReceiver() with given session", async () => {
+    const connection = new Connection({
+      port: mockServiceListener.address().port,
+    });
+
+    await connection.open();
+    assert.isTrue(connection.isOpen(), "Connection should be open.");
+
+    const session = await connection.createSession();
+    assert.isTrue(session.isOpen(), "Session should be open.");
+
+    const receiver = await connection.createReceiver({ session });
+    assert.isTrue(receiver.isOpen(), "Receiver should be open.");
+    assert.equal(receiver.session, session, "Session of receiver should be the same as the session passed to createReceiver()");
+
+    await connection.close();
+    assert.isFalse(receiver.isOpen(), "Receiver should be not be open after connection closed.");
+  });
+
+  it("createRequestResponseLink()", async () => {
+    const connection = new Connection({
+      port: mockServiceListener.address().port,
+    });
+
+    await connection.open();
+    assert.isTrue(connection.isOpen(), "Connection should be open.");
+
+    const requestResponseLink = await connection.createRequestResponseLink({}, {});
+
+    assert.isTrue(requestResponseLink.session.isOpen(), "Session should be open.");
+    assert.isTrue(requestResponseLink.receiver.isOpen(), "Receiver should be open.");
+    assert.isTrue(requestResponseLink.sender.isOpen(), "Sender should be open.");
+
+    await connection.close();
+    assert.isFalse(requestResponseLink.session.isOpen(), "Session should be not be open after connection closed.");
+    assert.isFalse(requestResponseLink.receiver.isOpen(), "Receiver should be not be open after connection closed.");
+    assert.isFalse(requestResponseLink.sender.isOpen(), "Sender should be not be open after connection closed.");
+  });
+
+  it("createRequestResponseLink() with given session", async () => {
+    const connection = new Connection({
+      port: mockServiceListener.address().port,
+    });
+
+    await connection.open();
+    assert.isTrue(connection.isOpen(), "Connection should be open.");
+
+    const session = await connection.createSession();
+    assert.isTrue(session.isOpen(), "Session should be open.");
+
+    const requestResponseLink = await connection.createRequestResponseLink({}, {}, session);
+
+    assert.isTrue(requestResponseLink.session.isOpen(), "Session should be open.");
+    assert.isTrue(requestResponseLink.receiver.isOpen(), "Receiver should be open.");
+    assert.isTrue(requestResponseLink.sender.isOpen(), "Sender should be open.");
+    assert.equal(requestResponseLink.session, session, "Session of requestResponseLink should be the same as the session passed to createRequestResponseLink()");
+
+    await connection.close();
+    assert.isFalse(requestResponseLink.session.isOpen(), "Session should be not be open after connection closed.");
+    assert.isFalse(requestResponseLink.receiver.isOpen(), "Receiver should be not be open after connection closed.");
+    assert.isFalse(requestResponseLink.sender.isOpen(), "Sender should be not be open after connection closed.");
+  });
+
   describe("supports events", () => {
     it("connectionOpen", (done: Function) => {
       const connection = new Connection({

3c03fe01c2bc8cbbca7cb13b6c4b6b4ca1be8383
HarshaNalluru <sanallur@microsoft.com>
2021-02-05 03:31:32 -0800

    porting to 3.5.1 TS

--- test/sender.spec.ts
+++ test/sender.spec.ts
@@ -75,10 +75,11 @@ describe("Sender", () => {
     mockService.on(
       rhea.ReceiverEvents.receiverOpen,
       (context: rhea.EventContext) => {
-        context.receiver?.close({
-          condition: errorCondition,
-          description: errorDescription,
-        });
+        context.receiver &&
+          context.receiver.close({
+            condition: errorCondition,
+            description: errorDescription,
+          });
       }
     );
 
@@ -119,10 +120,11 @@ describe("Sender", () => {
       mockService.on(
         rhea.ReceiverEvents.receiverClose,
         (context: rhea.EventContext) => {
-          context.receiver?.close({
-            condition: errorCondition,
-            description: errorDescription,
-          });
+          context.receiver &&
+            context.receiver.close({
+              condition: errorCondition,
+              description: errorDescription,
+            });
         }
       );
 
